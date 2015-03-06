bcbccec749ccb09860a13d94783235c2f91c860f
Haomiao Jiang <hjiang36@gmail.com>
2015-03-06 09:28:24 -0800

    add plotting function

---
+++ Functions/Data Process & Plot/coneContrast2DeltaEab.m
@@ -0,0 +1,18 @@
+function deltaE = coneContrast2DeltaEab(contrast, direction, d, bgColor)
+%% function coneContrast2DeltaEab(contrast, direction, d, bgColor)
+%    compute deltaE value for given cone contrast at given color direction
+%    in LMS space
+%
+%  Inputs:
+%    contrast  - contrast distance defined as Euclidean distance in LMS
+%                space
+%    direction - color direction in LMS space, should be a 2 element
+%                vector, including theta and phi
+%    d         - ISET display structure
+%    bgColor   - background color in RGB, default [0.5 0.5 0.5]
+%
+%  (HJ) ISETBIO TEAM, 2015
+
+%% 
+
+end
