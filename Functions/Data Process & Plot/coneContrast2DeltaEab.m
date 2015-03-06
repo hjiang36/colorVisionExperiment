68a58361ddd1042a22fd155716a697cfe33986eb
Haomiao Jiang <hjiang36@gmail.com>
2015-03-06 15:15:52 -0800

    Adding plotting routine

--- Functions/Data Process & Plot/coneContrast2DeltaEab.m
+++ Functions/Data Process & Plot/coneContrast2DeltaEab.m
@@ -1,18 +1,42 @@
-function deltaE = coneContrast2DeltaEab(contrast, direction, d, bgColor)
-%% function coneContrast2DeltaEab(contrast, direction, d, bgColor)
+function deltaE = coneContrast2DeltaEab(contrast, dir, d, cbType, bgColor)
+%% function coneContrast2DeltaEab(contrast, dir, d, bgColor)
 %    compute deltaE value for given cone contrast at given color direction
 %    in LMS space
 %
 %  Inputs:
 %    contrast  - contrast distance defined as Euclidean distance in LMS
 %                space
-%    direction - color direction in LMS space, should be a 2 element
-%                vector, including theta and phi
+%    dir       - color direction in LMS space, should be a 3 element unit
+%                length vector 
 %    d         - ISET display structure
+%    cbType    - colorblind type
 %    bgColor   - background color in RGB, default [0.5 0.5 0.5]
 %
 %  (HJ) ISETBIO TEAM, 2015
 
-%% 
+%% Check inputs & Init
+if notDefined('contrast'), error('Contrast required'); end
+if notDefined('dir'), error('direction required'); end
+if notDefined('d'), error('display structure required'); end
+if notDefined('cbType'), cbType = 0; end
+if notDefined('bgColor'), bgColor = [0.5 0.5 0.5]; end % half gray
+
+%% Computes stimulus color
+%  Compute stimulus contrast
+sContrast = contrast * dir;
+
+%  Compute background RGB values
+sRGB = coneContrast2RGB(d, sContrast, bgColor);
+
+%  Convert to XYZ
+sXYZ  = sRGB(:)' * displayGet(d, 'rgb2xyz');
+bgXYZ = bgColor(:)' * displayGet(d, 'rgb2xyz');
+
+%  Convert for colorblind
+sXYZ  = lms2xyz(xyz2lms(reshape(sXYZ, [1 1 3]), cbType, bgXYZ));
+bgXYZ = lms2xyz(xyz2lms(reshape(bgXYZ, [1 1 3]), cbType, bgXYZ));
+
+%% Compute deltaEab value
+deltaE = deltaEab(sXYZ, bgXYZ, bgXYZ);
 
 end
--- Functions/Data Process & Plot/s_plotDataEab.m
+++ Functions/Data Process & Plot/s_plotDataEab.m
@@ -14,16 +14,40 @@
 ieInit; % init a new ISET session
 
 %% Load data & parameters
+% fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
+%                    'expResults', 'hj', 'colorDetection_LS_Deuteran.mat');  
 fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
-                    'expResults', 'hj', 'colorDetection_LS_Deuteran.mat');
-load(fileName);
+                   'expResults', 'Final Results', 'Detection MS Plain',...
+                   'colorDetection_HJ_Protan.mat');
+control1 = load(fileName);
+
+% fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
+%                    'expResults', 'jf', 'colorDetection_LS_Deuteran.mat');
+fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
+                    'expResults', 'Final Results', 'Detection MS Plain',...
+                    'colorDetection_JEF_Protan.mat');
+control2 = load(fileName);
+
+% fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
+%                      'expResults', 'rz', 'colorDetection_LS.mat');
+fileName = fullfile(colorVisionExperimentRootPath, 'Exp Data', ...
+                   'expResults', 'Final Results', 'Detection MS Plain',...
+                    'colorDetection_Protanopia.mat');
+subjectData = load(fileName);
+
 direction = [30 45 75 105 135 150 -30 -45 -75 -105 -135 -150];
 pCorrect = 0.5:0.01:0.99;
 refContrast = [0 0 0]';
+d = displayCreate('CRT-NEC');
+cbType = 1; % Colorblind Type
 
 %% Plot for each direction
 % open new figure window
-vcNewGraphWin; 
+vcNewGraphWin; hold on;
+lineColorB = [117 112 179]/255;
+lineColorR = [217 95 2] / 255;
+lineColorG = [27 158 119] / 255;
+lineColorGray = [0.5 0.5 0.5];
 
 % plot for each color direction
 for ii = 1 : length(expResult)
@@ -31,7 +55,38 @@ for ii = 1 : length(expResult)
     subplot(3, 4, ii); grid on; hold on;
     title(sprintf('Direction:%d', direction(ii)));
     
-    sData = expResult(ii);
+    
+    sData = subjectData.expResult(ii);
+    dir = [0 cosd(direction(ii)) sind(direction(ii))];
+    indx = sData.numTrials > 3;
+    
+    % Weibull fit
+    [alpha,beta,~] = FitWeibAlphTAFC(sData.stimLevels(indx), ...
+        sData.numCorrect(indx), sData.numTrials(indx) - ...
+        sData.numCorrect(indx),[],4);
+    
+    % plot fitted curve
+    xContrast = alpha*(-log(2*(1-pCorrect))).^(1/beta);
+    xDeltaE = xContrast;
+    for jj = 1 : length(xContrast)
+        xDeltaE(jj) = coneContrast2DeltaEab(xContrast(jj), dir, d, cbType);
+    end
+    plot(xDeltaE, pCorrect, 'lineWidth', 2, 'Color', lineColorG);
+    
+    % convert contrast (stimLevels) to deltaE value
+    idx = find(indx);
+    xDeltaE = zeros(length(idx), 1);
+    for jj = 1 : length(idx)
+        xDeltaE(jj) = coneContrast2DeltaEab(sData.stimLevels(idx(jj)), ...
+                      dir, d, cbType);
+    end
+    
+    % scatter plot points
+    scatter(xDeltaE, sData.numCorrect(indx)./sData.numTrials(indx), ...
+            sData.numTrials(indx) * 4, lineColorG);
+        
+        
+    sData = control1.expResult(ii);
     dir = deg2rad(direction(ii));
     dir = [cos(dir) 0 sin(dir)];
     indx = sData.numTrials > 3;
@@ -42,19 +97,57 @@ for ii = 1 : length(expResult)
         sData.numCorrect(indx),[],4);
     
     % plot fitted curve
-    plot(alpha*(-log(2*(1-pCorrect))).^(1/beta), pCorrect);
+    xContrast = alpha*(-log(2*(1-pCorrect))).^(1/beta);
+    xDeltaE = xContrast;
+    for jj = 1 : length(xContrast)
+        xDeltaE(jj) = coneContrast2DeltaEab(xContrast(jj), dir, d, cbType);
+    end
+    plot(xDeltaE, pCorrect, '--', 'lineWidth', 1, 'Color', lineColorGray);
     
     % convert contrast (stimLevels) to deltaE value
-    %
-    %
-    %
+    idx = find(indx);
+    xDeltaE = zeros(length(idx), 1);
+    for jj = 1 : length(idx)
+        xDeltaE(jj) = coneContrast2DeltaEab(sData.stimLevels(idx(jj)), ...
+                      dir, d, cbType);
+    end
     
     % scatter plot points
-    scatter(sData.stimLevels(indx), ...
-            sData.numCorrect(indx)./sData.numTrials(indx), ...
-            sData.numTrials(indx) * 4, 'b');
+    scatter(xDeltaE, sData.numCorrect(indx)./sData.numTrials(indx), ...
+            sData.numTrials(indx) * 4, lineColorGray);
+        
+        
+    sData = control2.expResult(ii);
+    dir = deg2rad(direction(ii));
+    dir = [cos(dir) 0 sin(dir)];
+    indx = sData.numTrials > 3;
+    
+    % Weibull fit
+    [alpha,beta,~] = FitWeibAlphTAFC(sData.stimLevels(indx), ...
+        sData.numCorrect(indx), sData.numTrials(indx) - ...
+        sData.numCorrect(indx),[],4);
+    
+    % plot fitted curve
+    xContrast = alpha*(-log(2*(1-pCorrect))).^(1/beta);
+    xDeltaE = xContrast;
+    for jj = 1 : length(xContrast)
+        xDeltaE(jj) = coneContrast2DeltaEab(xContrast(jj), dir, d, cbType);
+    end
+    plot(xDeltaE, pCorrect, '--', 'lineWidth', 1, 'Color', lineColorGray);
+    
+    % convert contrast (stimLevels) to deltaE value
+    idx = find(indx);
+    xDeltaE = zeros(length(idx), 1);
+    for jj = 1 : length(idx)
+        xDeltaE(jj) = coneContrast2DeltaEab(sData.stimLevels(idx(jj)), ...
+                      dir, d, cbType);
+    end
+    
+    % scatter plot points
+    scatter(xDeltaE, sData.numCorrect(indx)./sData.numTrials(indx), ...
+            sData.numTrials(indx) * 4, lineColorGray);
     
     % set x and y limit
-    xlim([0 0.04]); ylim([0.5 1]);
+    xlim([0 1.5]); ylim([0.5 1]); drawnow();
     
 end
